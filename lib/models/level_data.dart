b0a89ad69523d3a25386f163e7369c8f8a1b2a9b
adrien04hz <adrien.ahs@gmail.com>
2026-02-09 17:23:46 -0600

    [ADD] LevelData model

---
+++ lib/models/level_data.dart
@@ -0,0 +1,11 @@
+class LevelData {
+  final int rows;
+  final int columns;
+  final List<List<int>> grid;
+
+  LevelData({
+    required this.rows,
+    required this.columns,
+    required this.grid,
+  });
+}
