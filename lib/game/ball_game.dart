5bdca3dd6eac75249320c3184d9385db804d472e
adrien04hz <adrien.ahs@gmail.com>
2026-02-09 17:38:37 -0600

    [ADD] Level generator in BallGame class

--- lib/game/ball_game.dart
+++ lib/game/ball_game.dart
@@ -2,40 +2,88 @@ import 'package:flame/game.dart';
 import 'game_state.dart';
 import 'systems/timer_system.dart';
 import 'package:ball_game/utils/constants.dart';
-
+import 'package:flame/components.dart';
+import 'levels/level_generator.dart';
+import 'components/wall.dart';
+import 'package:ball_game/models/level_data.dart';
 
 class BallGame extends FlameGame {
-    late final GameState gameState;
+  late final GameState gameState;
 
-    @override
-    Future<void> onLoad() async {
-      super.onLoad();
+  late LevelData currentLevelData;
+  final List<Wall> _walls = [];
 
-      final timer = TimerSystem( initialTime: initialGameTime );
-      gameState = GameState( timer: timer );
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
 
-      gameState.startGame();
+    final timer = TimerSystem( initialTime: initialGameTime );
+    gameState = GameState( timer: timer );
+
+    gameState.startGame();
+
+    loadLevel( gameState.currentLevel );
+  }
+
+  @override
+  void update( double dt ) {
+    super.update( dt );
+
+    gameState.update( dt );
+
+    if ( gameState.levelCompleted ) {
+      pauseEngine();
+      overlays.add( overlayLevelComplete );
     }
 
-    @override
-    void update( double dt ) {
-      super.update( dt );
+    if ( gameState.isGameOver ) {
+      pauseEngine();
+      overlays.add( overlayGameOver );
+    }
 
-      gameState.update( dt );
+    if ( gameState.isVictory ) {
+      pauseEngine();
+      overlays.add( overlayVictory );
+    }
+  }
 
-      if ( gameState.levelCompleted ) {
-        pauseEngine();
-        overlays.add( overlayLevelComplete );
-      }
+  // Cargar nivel
+  void loadLevel( int level ) {
+    // Limpiar paredes anteriores
+    for ( final wall in _walls ) {
+      wall.removeFromParent();
+    }
 
-      if ( gameState.isGameOver ) {
-        pauseEngine();
-        overlays.add( overlayGameOver );
-      }
+    _walls.clear();
 
-      if ( gameState.isVictory ) {
-        pauseEngine();
-        overlays.add( overlayVictory );
+    // Generar nuevo nivel
+    currentLevelData = LevelGenerator.generate( level );
+
+    final rows = currentLevelData.rows;
+    final cols = currentLevelData.columns;
+
+    // Tamaño de cada celda segun pantalla
+    final cellWidth = size.x / cols;
+    final cellHeight = size.y / rows;
+
+    for ( int r = 0; r < rows; r++ ) {
+      for ( int c = 0; c < cols; c++ ) {
+        if ( currentLevelData.grid[r][c] == 1 ) {
+          final wall = Wall(
+            position: Vector2(
+              c * cellWidth,
+              r * cellHeight,
+            ),
+            size: Vector2(
+              cellWidth,
+              cellHeight,
+            ),
+          );
+
+          _walls.add( wall );
+          add( wall );
+        }
       }
     }
+  }
 }
