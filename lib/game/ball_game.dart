248dcc68829996005664a44ce373a9ac6991f9e5
adrien04hz <adrien.ahs@gmail.com>
2026-02-09 15:19:01 -0600

    [ADD] BallGame class

--- lib/game/ball_game.dart
+++ lib/game/ball_game.dart
@@ -1,1 +1,35 @@
 import 'package:flame/game.dart';
+import 'game_state.dart';
+import 'systems/timer_system.dart';
+
+
+class BallGame extends FlameGame {
+    late final GameState gameState;
+
+    @override
+    Future<void> onLoad() async {
+        super.onLoad();
+
+        final timer = TimerSystem( initialTime: 300 );
+        gameState = GameState( timer: timer );
+
+        gameState.startGame();
+    }
+
+    @override
+    void update( double dt ) {
+        super.update( dt );
+
+        gameState.update( dt );
+
+        if ( gameState.isGameOver ) {
+            pauseEngine();
+            overlays.add( 'GameOver' );
+        }
+
+        if ( gameState.isVictory ) {
+            pauseEngine();
+            overlays.add( 'Victory' );
+        }
+    }
+}
