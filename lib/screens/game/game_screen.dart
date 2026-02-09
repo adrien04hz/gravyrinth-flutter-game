71e5aec982507fd6a83250b935ba4e9d1bc7cfb0
adrien04hz <adrien.ahs@gmail.com>
2026-02-09 15:36:24 -0600

    [ADD] GameScreen class for HUD and UI

---
+++ lib/screens/game/game_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import '../../game/ball_game.dart';
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({ super.key });
+
+  @override
+  Widget build( BuildContext context ) {
+    final game = BallGame();
+
+    return Scaffold(
+      body: GameWidget<BallGame>(
+        game: game,
+        overlayBuilderMap: {
+          'HUD': ( context, BallGame game) {
+            return _Hud( game: game );
+          },
+          'GameOver': ( context, BallGame game ) {
+            return const Center(
+              child: Text(
+                'GAME OVER',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.red,
+                ),
+              ),
+            );
+          },
+          'Victory': ( context, BallGame game ) {
+            return const Center(
+              child: Text(
+                '¡GANASTE!',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.green,
+                ),
+              ),
+            );
+          },
+        },
+        initialActiveOverlays: const ['HUD'],
+      ),
+    );
+  }
+}
+
+
+class _Hud extends StatelessWidget {
+  final BallGame game;
+
+  const _Hud({ required this.game });
+
+  @override
+  Widget build( BuildContext context ) {
+    final time = game.gameState.timer.timeRemaining;
+
+    final minutes = ( time ~/ 60).toString().padLeft( 2, '0' );
+    final seconds = ( time % 60).toString().padLeft( 2, '0' );
+
+    return Positioned(
+      top: 40,
+      left: 20,
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.black.withValues(alpha: 0.6),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(
+          '$minutes:$seconds',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
