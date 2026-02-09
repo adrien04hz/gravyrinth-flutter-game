a2f4a2a75fe45cb92378e59a4e000b8463614f33
adrien04hz <adrien.ahs@gmail.com>
2026-02-09 16:01:06 -0600

    [FIX] Timer is workinggit add .!

--- lib/screens/game/game_screen.dart
+++ lib/screens/game/game_screen.dart
@@ -53,28 +53,31 @@ class _Hud extends StatelessWidget {
 
   @override
   Widget build( BuildContext context ) {
-    final time = game.gameState.timer.timeRemaining;
+    return ValueListenableBuilder<double>(
+      valueListenable: game.gameState.timer.timeNotifier,
+      builder: ( context, time, _ ) {
+        final minutes = ( time ~/ 60).toString().padLeft( 2, '0' );
+        final seconds = ( time % 60).toInt().toString().padLeft( 2, '0' );
 
-    final minutes = ( time ~/ 60).toString().padLeft( 2, '0' );
-    final seconds = ( time % 60).toString().padLeft( 2, '0' );
-
-    return Positioned(
-      top: 40,
-      left: 20,
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: Colors.black.withValues(alpha: 0.6),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Text(
-          '$minutes:$seconds',
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 24,
+        return Positioned(
+          top: 40,
+          left: 20,
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.black.withValues(alpha: 0.6),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Text(
+              '$minutes:$seconds',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
