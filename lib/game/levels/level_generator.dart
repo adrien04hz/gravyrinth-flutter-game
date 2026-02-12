e510cc15e8a81dc3d80c4a6de5eb3db38bc97eb4
adrien04hz <adrien.ahs@gmail.com>
2026-02-11 19:23:21 -0600

    [ADD] New dfs generator and cell manager

--- lib/game/levels/level_generator.dart
+++ lib/game/levels/level_generator.dart
@@ -20,4 +20,49 @@ class LevelGenerator {
       grid: grid,
     );
   }
+
+  static void _generateMaze( List<List<Cell>> grid, int rows, int cols  ) {
+    final random = Random();
+
+    void dfs( int r, int c ) {
+      grid[r][c].visited = true;
+
+      final directions = [
+        [0, 1], // derecha
+        [1, 0], // abajo
+        [0, -1], // izquierda
+        [-1, 0], // arriba
+      ]..shuffle( random );
+
+      for ( final dir in directions ) {
+        final nr = r + dir[0];
+        final nc = c + dir[1];
+
+        if ( nr >= 0 && 
+            nc >= 0 && 
+            nr < rows && 
+            nc < cols && 
+            !grid[nr][nc].visited ) {
+          
+          // Romper paredes entre celdas
+          if ( dir[0] == 0 && dir[1] == 1 ) {
+            grid[r][c].right = false;
+            grid[nr][nc].left = false;
+          } else if ( dir[0] == 1 && dir[1] == 0 ) {
+            grid[r][c].bottom = false;
+            grid[nr][nc].top = false;
+          } else if ( dir[0] == 0 && dir[1] == -1 ) {
+            grid[r][c].left = false;
+            grid[nr][nc].right = false;
+          } else if ( dir[0] == -1 && dir[1] == 0 ) {
+            grid[r][c].top = false;
+            grid[nr][nc].bottom = false;
+          }
+
+          dfs( nr, nc );
+        }
+      }
+    }
+    dfs( 0, 0 );
+  }
 }
