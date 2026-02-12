c964ca6856cdcb730d1d791d7cd4218adfe83b55
adrien04hz <adrien.ahs@gmail.com>
2026-02-11 20:39:18 -0600

    [ADD] Ball class

---
+++ lib/game/components/ball.dart
@@ -0,0 +1,24 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flutter/material.dart';
+
+class Ball extends CircleComponent {
+  Ball({
+    required Vector2 position,
+    required double radius,
+  }) : super (
+    position: position,
+    radius: radius,
+    paint: Paint()..color = Colors.orange,
+    anchor: Anchor.center,
+  );
+
+  Vector2 velocity = Vector2.zero();
+
+  @override
+  void update( double dt ) {
+    super.update( dt );
+
+    position += velocity * dt;
+  }
+}
