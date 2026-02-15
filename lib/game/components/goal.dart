a81a22f0748d9f148393a7b0e1d60d00211374a3
adrien04hz <adrien.ahs@gmail.com>
2026-02-15 00:45:19 -0600

    [ADD] Goal class

---
+++ lib/game/components/goal.dart
@@ -0,0 +1,13 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Goal extends RectangleComponent {
+  Goal({
+    required Vector2 position,
+    required Vector2 size,
+  }) : super(
+    position: position,
+    size: size,
+    paint: Paint()..color = Colors.greenAccent,
+  );
+}
